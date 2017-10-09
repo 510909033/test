f7290305f5cdbabf4b9be9cfe24def1bf2515b57
510909033 <510909033@qq.com>
2017-10-09 14:26:36 +0800

    Signed-off-by: 510909033 <510909033@qq.com>

--- newfile.php
+++ newfile.php
@@ -1,9 +1,93 @@
 <?php
-sss
+ for ($i = 0; $i<100000;$i++){
+
+    $arr[] = rand(0,100000);
+}
+
+ $t1 = microtime(true);
+ $step=0;
+$returnAr = quickSort($arr);
+
+echo $step."\n";
+
+$t2 = microtime(true);
+echo "usetime:".($t2-$t1);
+$i=0;
+foreach ($returnAr as $k=>$v){
+    echo $v."\n";
+    if ($i++ > 10  ){
+        return ;
+    }
+}
+ 
+//快速排序
+function quickSort($arr) { 
+    global $step;
+    $step++;
+    //先判断是否需要继续进行
+    $length = count($arr);
+    if($length <= 1) {
+        return $arr;
+    }
+    //选择第一个元素作为基准
+    $base_num = $arr[0];
+    //遍历除了标尺外的所有元素，按照大小关系放入两个数组内
+    //初始化两个数组
+    $left_array = array();  //小于基准的
+    $right_array = array();  //大于基准的
+    for($i=1; $i<$length; $i++) {
+        if($base_num > $arr[$i]) {
+            //放入左边数组
+            $left_array[] = $arr[$i];
+        } else {
+            //放入右边
+            $right_array[] = $arr[$i];
+        }
+    }
+    //再分别对左边和右边的数组进行相同的排序处理方式递归调用这个函数
+    $left_array = quickSort($left_array);
+    $right_array = quickSort($right_array);
+    //合并
+    return array_merge($left_array, array($base_num), $right_array);
+}
 
 
-ssss
+
+function quickSort1($arr) {
+    //先判断是否需要继续进行
+    $length = count($arr);
+    if($length <= 1) {
+        return $arr;
+    }
+    
+    
+    
+    
+    
+    //选择第一个元素作为基准
+    $base_num = $arr[0];
+    //遍历除了标尺外的所有元素，按照大小关系放入两个数组内
+    //初始化两个数组
+    $left_array = array();  //小于基准的
+    $right_array = array();  //大于基准的
+    for($i=1; $i<$length; $i++) {
+        if($base_num > $arr[$i]) {
+            //放入左边数组
+            $left_array[] = $arr[$i];
+        } else {
+            //放入右边
+            $right_array[] = $arr[$i];
+        }
+    }
+    //再分别对左边和右边的数组进行相同的排序处理方式递归调用这个函数
+    $left_array = quickSort($left_array);
+    $right_array = quickSort($right_array);
+    //合并
+    return array_merge($left_array, array($base_num), $right_array);
+}
 
 
 
-sssss
+
+
+
